cb33f4595339a4c0def4f91a86bf6d76b14abdea
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-01 19:09:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_accessor :total, :discount, :titles
 
+  def initialize(employee_discount = 0)
+    @total, @titles = 0, []
+    @discount = employee_discount
+  end
+
+  def add_item(title,price, quantities = 1)
+    @total += price * quantities
+    @titles << title
+  end
+
+  def apply_discount
+    return "There is no discount to apply." if @discount == 0
+    @total -= (@discount * @total) / 100.0
+    "After the discount, the total comes to $#{@total.to_i}."
+  end
+
+  def items
+    @titles
+  end
+end
